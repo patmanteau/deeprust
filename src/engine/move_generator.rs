5052c0079b18928fd17c4d64569056f6385c978a
Patrick Haas <patmanteau@posteo.de>
2017-12-05 20:26:56 +0100

    some tests

--- src/engine/move_generator.rs
+++ src/engine/move_generator.rs
@@ -441,7 +441,39 @@ mod tests {
         assert_eq!(3, gen.gen_king_moves(&board).len());
     }
 
+    #[test]
+    fn it_generates_all_moves() {
+        let mut gen = MoveGenerator::new();
 
-    
-    
+        // see https://chessprogramming.wikispaces.com/Perft+Results
+        // Position 1(startpos) perft(1)
+        let mut board = Board::startpos();
+        gen.from_board(&board);
+        assert_eq!(20, gen.moves().len());
+
+        // Position 2(kiwipete) perft(1)
+        board = Board::from_fen(String::from("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1")).unwrap();
+        gen.from_board(&board);
+        assert_eq!(48, gen.moves().len());
+
+        // // Position 3 perft(1)
+        // board = Board::from_fen(String::from("8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1")).unwrap();
+        // gen.from_board(&board);
+        // assert_eq!(14, gen.moves().len());
+
+        // // Position 4 perft(1)
+        // board = Board::from_fen(String::from("r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1")).unwrap();
+        // gen.from_board(&board);
+        // assert_eq!(6, gen.moves().len());
+
+        // // Position 5 perft(1)
+        // board = Board::from_fen(String::from("rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8")).unwrap();
+        // gen.from_board(&board);
+        // assert_eq!(44, gen.moves().len());
+
+        // Position 6 perft(1)
+        board = Board::from_fen(String::from("r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10")).unwrap();
+        gen.from_board(&board);
+        assert_eq!(46, gen.moves().len());
+    }
 }
