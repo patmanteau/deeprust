60b0341a8fa650f36ccfc380a9def6c510b90282
Patrick Haas <patmanteau@posteo.de>
2019-07-02 21:01:06 +0200

    Colorful batch perfting

--- src/main.rs
+++ src/main.rs
@@ -32,23 +32,28 @@ pub mod uci;
 // use clap::{App, Arg};
 use board::Board;
 use interfaces::FenInterface;
-use search::{PerftContext, Search};
+use search::Search;
 
+use ansi_term::Colour::{Red, Green, Cyan};
 use log::LevelFilter;
 use std::error::Error;
 use std::fs::File;
-use std::io::{BufRead, BufReader};
+use std::io::{self, BufRead, BufReader, Write};
 use std::path::PathBuf;
-use std::str::FromStr;
+// use std::str::FromStr;
 
-fn selftest(f: &str) {
-    let mut path = PathBuf::from(f);
+fn batchperft(f: &str) {
+    let path = PathBuf::from(f);
     let file = match File::open(&path) {
         Err(why) => panic!("Could not open {}: {}", path.display(), why.description()),
         Ok(file) => file,
     };
 
-    debug!("Running selftest from {}", path.to_str().unwrap());
+    println!("Running batch perft from {}", path.to_str().unwrap());
+    debug!("Running batch perft from {}", path.to_str().unwrap());
+
+    let (mut num_ok, mut num_error) = (0, 0);
+
     for (num, line) in BufReader::new(file).lines().map(|l| l.unwrap()).enumerate() {
         if line.trim().is_empty() || line.trim().starts_with('#') {
             continue;
@@ -58,7 +63,9 @@ fn selftest(f: &str) {
             let fen_str = parts[0];
             let mut b = Board::from_fen_str(&fen_str).unwrap();
 
-            println!("Testing fen {}...", fen_str);
+            let mut ok = true;
+
+            print!("perft '{}' ", fen_str);
             for depth_def in &parts[1..] {
                 let depth_def_parts: Vec<u64> = 
                     depth_def
@@ -67,16 +74,31 @@ fn selftest(f: &str) {
                     .collect();
                 let (depth, nodes) = (depth_def_parts[0], depth_def_parts[1]);
                 // print!("Running perft {} on '{}' expecting {} nodes...", depth, fen_str, nodes);
+                print!("{} ", depth);
+                io::stdout().flush().unwrap();
                 let res = b.perft(depth as u32);
                 if res.nodes == nodes {
                     // println!("OK");
                 } else {
-                    println!("Line {} failed at depth {}: got {} instead of {} nodes", num + 1, depth, res.nodes, nodes);
+                    ok = false;
+                    // println!("Line {} failed at depth {}: got {} instead of {} nodes", num + 1, depth, res.nodes, nodes);
                     debug!("Line {} failed at depth {}: got {} instead of {} nodes", num + 1, depth, res.nodes, nodes);
                 }
             }
+
+            if ok {
+                println!("{}", Green.bold().paint("[OK]"));
+                num_ok += 1;
+            } else {
+                println!("{}", Red.bold().paint("[Error]"));
+                num_error += 1;
+            }
+            io::stdout().flush().unwrap();
         }
     }
+
+    println!("Finished batch perft: {} passed, {} failed.", num_ok, num_error);
+    debug!("Finished batch perft: {} passed, {} failed.", num_ok, num_error);
 }
 
 fn main() {
@@ -101,13 +123,14 @@ fn main() {
     //             .takes_value(true),
     //     )
     //     .get_matches();
+    let _enabled = ansi_term::enable_ansi_support();
 
     let matches = clap_app!(deeprust =>
         (version: env!("CARGO_PKG_VERSION"))
         (author: env!("CARGO_PKG_AUTHORS"))
         (about: "A chess playing program.")
         (@arg cli: -c --cli "Starts in CLI mode")
-        (@arg FENFILE: -s --selftest +takes_value "Runs perfts from a FEN file")
+        (@arg FENFILE: -b --batch +takes_value "Runs perfts from a FEN file")
         (@arg debug: -d ... "Sets the level of debugging information")
     ).get_matches();
 
@@ -115,7 +138,7 @@ fn main() {
         cli::run();
     } else if matches.is_present("FENFILE") {
         let config = matches.value_of("FENFILE").unwrap_or("tests/perftsuite.epd");
-        selftest(config);
+        batchperft(config);
     }else {
         let mut c = uci::UCIInterface::new();
         c.run();
